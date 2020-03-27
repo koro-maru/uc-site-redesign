81f88e29424260efa38c028a49fdc847f7b5de24
koro-maru <sotoemily03@gmail.com>
2020-03-27 18:22:28 -0400

    Small edits

--- ucWebRedesign/routes/index.js
+++ ucWebRedesign/routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/home.html')
- // res.render('index', { title: 'Express' });
+  res.send("home.html")
 });
 
 module.exports = router;
